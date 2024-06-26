eb112f74a267c5a2b54d07c8cecda33bc207904e
paulalextech <psteane123@hotmail.com>
2024-06-26 13:41:11 +0100

    first commit

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+class Calculator
+{
+    static void Main(string [] args)
+    {
+        bool keepRunning = true;
+        while (keepRunning)
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the operator (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            switch(operation);
+            {
+                
+            }
+        }
+    }
+}
+
+
+
+// menu
+
+
+
+// do-while loop to print menu
+
+
+// conditional to end loop
+
+
+// numbered items
+
+
+// switch statement
+
+
+// Console.ReadLine() user input step by step
+
+
+
+// research for help if needed.
